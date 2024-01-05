71ff4942c6af15e6afa2a9e6d0e976a65445235b
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-01-04 23:52:41 -0600

    Add files via upload

---
+++ resources/js/script-3.js
@@ -0,0 +1,87 @@
+function reproducirAudio1() {
+  var audio1 = document.getElementById("audio1");
+  if (audio1.paused) {
+    audio1.play();
+    document.getElementById("boton1").innerHTML = "Pausar lectura";
+  } else {
+    audio1.pause();
+    document.getElementById("boton1").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio2() {
+  var audio2 = document.getElementById("audio2");
+  if (audio2.paused) {
+    audio2.play();
+    document.getElementById("boton2").innerHTML = "Pausar lectura";
+  } else {
+    audio2.pause();
+    document.getElementById("boton2").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio3() {
+  var audio3= document.getElementById("audio3");
+  if (audio3.paused) {
+    audio3.play();
+    document.getElementById("boton3").innerHTML = "Pausar lectura";
+  } else {
+    audio3.pause();
+    document.getElementById("boton3").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio4() {
+  var audio4 = document.getElementById("audio4");
+  if (audio4.paused) {
+    audio4.play();
+    document.getElementById("boton4").innerHTML = "Pausar lectura";
+  } else {
+    audio4.pause();
+    document.getElementById("boton4").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio5() {
+  var audio5 = document.getElementById("audio5");
+  if (audio5.paused) {
+    audio5.play();
+    document.getElementById("boton5").innerHTML = "Pausar lectura";
+  } else {
+    audio5.pause();
+    document.getElementById("boton5").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio6() {
+  var audio6 = document.getElementById("audio6");
+  if (audio6.paused) {
+    audio6.play();
+    document.getElementById("boton6").innerHTML = "Pausar lectura";
+  } else {
+    audio6.pause();
+    document.getElementById("boton6").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio7() {
+  var audio7 = document.getElementById("audio7");
+  if (audio7.paused) {
+    audio7.play();
+    document.getElementById("boton7").innerHTML = "Pausar lectura";
+  } else {
+    audio7.pause();
+    document.getElementById("boton7").innerHTML = "Escuchar lectura";
+  }
+}
+
+function reproducirAudio8() {
+  var audio8 = document.getElementById("audio8");
+  if (audio8.paused) {
+    audio8.play();
+    document.getElementById("boton8").innerHTML = "Pausar lectura";
+  } else {
+    audio8.pause();
+    document.getElementById("boton8").innerHTML = "Escuchar lectura";
+  }
+}
