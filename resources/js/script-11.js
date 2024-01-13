f01a683fa46a7de1fb53c5a6d29b15d14eb47514
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-01-13 00:33:58 -0600

    Update script-11.js

--- resources/js/script-11.js
+++ resources/js/script-11.js
@@ -1,7 +1,9 @@
-function mostrarAdvertencia() {
-    document.getElementById('mensaje-modal').style.display = 'block';
+function mostrarModal() {
+    document.getElementById('fondoModal').style.display = 'block';
+    document.getElementById('miModal').style.display = 'block';
   }
 
-  function ocultarAdvertencia() {
-    document.getElementById('mensaje-modal').style.display = 'none';
+  function cerrarModal() {
+    document.getElementById('fondoModal').style.display = 'none';
+    document.getElementById('miModal').style.display = 'none';
   }
