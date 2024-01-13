c4320f81316be5d3c86de3f878ff63e1aeeea5d3
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-01-12 23:21:27 -0600

    Create script-11.js

---
+++ resources/js/script-11.js
@@ -0,0 +1,7 @@
+function mostrarAdvertencia() {
+    document.getElementById('mensaje').style.display = 'block';
+  }
+
+  function ocultarAdvertencia() {
+    document.getElementById('mensaje').style.display = 'none';
+  }
