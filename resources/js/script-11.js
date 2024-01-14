63d927340252788ef0819537d5f302ce173473d9
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-01-13 18:29:11 -0600

    Update script-11.js

--- resources/js/script-11.js
+++ resources/js/script-11.js
@@ -1,6 +1,26 @@
-function mostrarModal() {
+function mostrarModal(enlaceId) {
     document.getElementById('fondoModal').style.display = 'block';
     document.getElementById('miModal').style.display = 'block';
+    
+    if (enlaceId === 'Enlace-Pagina1') {
+      document.getElementById('contenidoModal').innerText = "Este enlace en la Página 1...";
+    }
+    
+  }
+
+  function cerrarModal() {
+    document.getElementById('fondoModal').style.display = 'none';
+    document.getElementById('miModal').style.display = 'none';
+  }
+
+  function mostrarModal(enlaceId) {
+    document.getElementById('fondoModal').style.display = 'block';
+    document.getElementById('miModal').style.display = 'block';
+    
+    if (enlaceId === 'Enlace-Pagina2') {
+      document.getElementById('contenidoModal').innerText = "Este enlace en la Página 1...";
+    }
+    
   }
 
   function cerrarModal() {
