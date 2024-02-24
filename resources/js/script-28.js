9d5dbc6b1535172b45d63752f5fce70f173a20fe
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-02-23 19:43:02 -0600

    Update script-28.js

--- resources/js/script-28.js
+++ resources/js/script-28.js
@@ -1,26 +1,30 @@
-    function changeLanguage() {
-      var langSelect = document.getElementById("lang");
-      var selectedLang = langSelect.value;
+        function changeLanguage() {
+            var langSelect = document.getElementById("lang");
+            var selectedLang = langSelect.value;
 
-      localStorage.setItem("language", selectedLang);
-      
-      if (selectedLang === "en") {
-        document.getElementById("englishText").style.display = "block";
-        document.getElementById("spanishText").style.display = "none";
-      } else {
-        document.getElementById("englishText").style.display = "none";
-        document.getElementById("spanishText").style.display = "block";
-      }
-    }
-    
-    var savedLang = localStorage.getItem("language");
-    
-    if (!savedLang) {
-      savedLang = "es";
-      localStorage.setItem("language", savedLang);
-    }
-    
-    var langSelect = document.getElementById("lang");
-    langSelect.value = savedLang;
-    
-    changeLanguage();
+            localStorage.setItem("language", selectedLang);
+
+            if (selectedLang === "en") {
+                document.getElementById("englishText").style.display = "block";
+                document.getElementById("spanishText").style.display = "none";
+            } else {
+                document.getElementById("englishText").style.display = "none";
+                document.getElementById("spanishText").style.display = "block";
+            }
+        }
+
+        var savedLang = localStorage.getItem("language");
+
+        if (!savedLang) {
+            savedLang = "es";
+        }
+ 
+        document.getElementById("lang").value = savedLang;
+
+        if (savedLang === "en") {
+            document.getElementById("englishText").style.display = "block";
+            document.getElementById("spanishText").style.display = "none";
+        } else {
+            document.getElementById("englishText").style.display = "none";
+            document.getElementById("spanishText").style.display = "block";
+        }
