ae65f7697738fb2e68e38d82eb449dbf4018834f
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-02-23 20:17:08 -0600

    Create script-28.js

---
+++ resources/js/script-28.js
@@ -0,0 +1,30 @@
+function changeLanguage() {
+            var langSelect = document.getElementById("lang");
+            var selectedLang = langSelect.value;
+
+            localStorage.setItem("language", selectedLang);
+
+            if (selectedLang === "en") {
+                document.getElementById("englishText").style.display = "block";
+                document.getElementById("spanishText").style.display = "none";
+            } else {
+                document.getElementById("englishText").style.display = "none";
+                document.getElementById("spanishText").style.display = "block";
+            }
+        }
+
+        var savedLang = localStorage.getItem("language");
+
+        if (!savedLang) {
+            savedLang = "es";
+        }
+
+        document.getElementById("lang").value = savedLang;
+
+        if (savedLang === "en") {
+            document.getElementById("englishText").style.display = "block";
+            document.getElementById("spanishText").style.display = "none";
+        } else {
+            document.getElementById("englishText").style.display = "none";
+            document.getElementById("spanishText").style.display = "block";
+        }
