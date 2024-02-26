be6b7712081bedcf76e245a7b7469d3d1c0f5c0b
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-02-25 18:40:48 -0600

    Update script-28.js

--- resources/js/script-28.js
+++ resources/js/script-28.js
@@ -1,16 +1,17 @@
-  const themeSelector = document.getElementById('theme');
-    const body = document.body;
-    const savedTheme = localStorage.getItem('theme');
+const themeSelector = document.getElementById('theme');
+const body = document.body;
+const savedTheme = localStorage.getItem('theme');
 
-    if (savedTheme) {
-        body.className = savedTheme;
-        
-        themeSelector.value = savedTheme;
-    }
+if (savedTheme) {
+    body.className = savedTheme;
+    themeSelector.value = savedTheme;
+} else {
+    body.className = 'light';
+    localStorage.setItem('theme', 'light');
+}
 
-    themeSelector.addEventListener('change', function() {
-        const selectedTheme = this.value;
-        body.className = selectedTheme;
-        
-        localStorage.setItem('theme', selectedTheme);
-    });
+themeSelector.addEventListener('change', function() {
+    const selectedTheme = this.value;
+    body.className = selectedTheme;
+    localStorage.setItem('theme', selectedTheme);
+});  
