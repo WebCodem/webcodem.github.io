740ffdbeb1551d0fdd4e43d811218386f65df1eb
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-04-13 17:45:03 -0600

    Create script-41.js

---
+++ resources/js/script-41.js
@@ -0,0 +1,21 @@
+  const formulario = document.getElementById("formulario");
+  const respuestas = formulario.querySelectorAll("input[type='radio']");
+
+  respuestas.forEach(respuesta => {
+    respuesta.addEventListener("change", () => {
+      const valor = respuesta.value;
+
+      let nextPage = "";
+      switch(valor) {
+        case "si":
+          nextPage = "./respuesta-3";
+          break;
+        case "no":
+          nextPage = "./respuesta-4";
+          break;
+      
+      }
+      
+      window.location.href = nextPage;
+    });
+  });
