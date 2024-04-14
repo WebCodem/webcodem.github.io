649b20fa77cd6c86412b1661cd72be4c3d7d6699
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-04-13 18:18:32 -0600

    Update script-42.js

--- resources/js/script-42.js
+++ resources/js/script-42.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-      const webhookUrl = "https://discord.com/api/webhooks/1228854747956641812/OB8KUE9Bk6MIfU7eetsycfb5JFPEyHdw-O0W2LLj5vuxZypL2IO9uKMiU5MNqewSQtsc";
+      const webhookUrl = "https://discord.com/api/webhooks/1228857141218115666/_5MWkbTCIO48eYjdk3fV4lMQyQNq74gqrEF5LSsy6jF6BPYdHwCZl7O9yrKtzzYo6isB";
       const message = "¡Lo siento mucho, tu novia no te prefiere a ti!";
 
       fetch(webhookUrl, {
