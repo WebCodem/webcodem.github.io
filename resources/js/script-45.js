91805d9ab664fa051ba03d753433face18d80474
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-04-13 18:36:59 -0600

    Create script-45.js

---
+++ resources/js/script-45.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+      const webhookUrl = "https://discord.com/api/webhooks/1228866240299270245/Gn6xl0DMn4aMzh8N_gKcNVYZZl2onhwuL0DM0aYtDclwbdLV2Vcfh2ewE0NBdS-2r9OO";
+      const message = "¡Lo siento mucho, pero tu novia tiene pretendientes!";
+
+      fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content: message }),
+      });
+    });
