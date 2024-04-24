3ad2fe59df4e377ca41f4c7344df5814e803a004
WebCodem <151910209+WebCodem@users.noreply.github.com>
2024-04-24 02:59:50 -0600

    Delete resources/js/script-54.js

--- resources/js/script-54.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("p").addEventListener("click", function() {
-      document.getElementById("overlay").style.display = "block";
-    });
-    
-    document.getElementById("close").addEventListener("click", function() {
-      document.getElementById("overlay").style.display = "none";
-    });
-  });
