f3a822d2d733b5395c582d88d22db848de1582d4
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-05-04 23:05:06 -0600

    Update test.js

--- api/test.js
+++ api/test.js
@@ -6,6 +6,13 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 function sumar(a, b) {
     return a + b;
 }
