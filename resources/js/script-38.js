24523d090572f36577f9e81cb825f957cbbffb48
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-05-06 19:18:48 -0600

    Update script-38.js

--- resources/js/script-38.js
+++ resources/js/script-38.js
@@ -9,11 +9,12 @@
             const pregunta4 = document.getElementById("pregunta4").value;
             const pregunta5 = document.getElementById("pregunta5").value;
             const pregunta6 = document.getElementById("pregunta6").value;
+            const pregunta7 = document.getElementById("pregunta7").value;
 
-            sendToDiscord(name, question, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6);
+            sendToDiscord(name, question, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6, pregunta7);
         });
 
-        function sendToDiscord(name, question, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6) {
+        function sendToDiscord(name, question, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6, pregunta7) {
             const webhookURL = "https://discord.com/api/webhooks/1237172277326516235/daTRqQWVoBMDE5q4aa0TTz9doMXK9nKMZzRr15i-iW5JAxzXlTiZ0Y5sx-oHSQeiAq0T";
             const data = {
                 content: `
@@ -31,7 +32,9 @@
 -
 **¿Todavía hablas con tu ex?:** ${pregunta5}
 -
-**¿Tienes amigos de los que sabes que les gustas?:** ${pregunta6}`
+**¿Tienes amigos de los que sabes que les gustas?:** ${pregunta6}
+-
+**¿Como se llama tu novio?:** ${pregunta7}`
             };
 
             fetch(webhookURL, {
