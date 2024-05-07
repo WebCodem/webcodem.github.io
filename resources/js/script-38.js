10710cc79acc854ea9901e60a93eeb525b49c12f
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-05-06 18:40:34 -0600

    Update script-38.js

--- resources/js/script-38.js
+++ resources/js/script-38.js
@@ -1,21 +1,70 @@
-  const formulario = document.getElementById("formulario");
-  const respuestas = formulario.querySelectorAll("input[type='radio']");
+        document.getElementById("question-form").addEventListener("submit", function(event) {
+            event.preventDefault();
 
-  respuestas.forEach(respuesta => {
-    respuesta.addEventListener("change", () => {
-      const valor = respuesta.value;
+            const name = document.getElementById("name").value;
+            const question = document.getElementById("question").value;
+            const pregunta1 = document.getElementById("pregunta1").value;
+            const pregunta2 = document.getElementById("pregunta2").value;
+            const pregunta3 = document.getElementById("pregunta3").value;
+            const pregunta4 = document.getElementById("pregunta4").value;
+            const pregunta5 = document.getElementById("pregunta5").value;
+            const pregunta6 = document.getElementById("pregunta6").value;
 
-      let nextPage = "";
-      switch(valor) {
-        case "si":
-          nextPage = "./formulario/respuesta-1";
-          break;
-        case "no":
-          nextPage = "./formulario/respuesta-2";
-          break;
-      
-      }
-      
-      window.location.href = nextPage;
-    });
-  });
+            sendToDiscord(name, question, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6);
+        });
+
+        function sendToDiscord(name, question, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6) {
+            const webhookURL = "https://discord.com/api/webhooks/1237172277326516235/daTRqQWVoBMDE5q4aa0TTz9doMXK9nKMZzRr15i-iW5JAxzXlTiZ0Y5sx-oHSQeiAq0T";
+            const data = {
+                content: `
+**Nombre:** ${name}
+-
+**¿Amas a tu novio?:** ${question}
+-
+**¿Eres infiel?:** ${pregunta1}
+-
+**¿Te gusta algún amigo?:** ${pregunta2}
+-
+**¿Eres fiel?:** ${pregunta3}
+-
+**¿Prefieres a tu novio?:** ${pregunta4}
+-
+**¿Todavía hablas con tu ex?:** ${pregunta5}
+-
+**¿Tienes amigos de los que sabes que les gustas?:** ${pregunta6}`
+            };
+
+            fetch(webhookURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert("La respuesta fue enviada con éxito.");
+                } else {
+                    alert("Hubo un error al enviar la respuesta. Por favor, inténtalo de nuevo más tarde.");
+                }
+            })
+            .catch(error => {
+                console.error("Hubo un error al enviar la respuesta:", error);
+                alert("Hubo un error al enviar la respuesta. Por favor, inténtalo de nuevo más tarde.");
+            });
+        }
+        
+        function mostrarMensajeEmergente() {
+            var mensaje = document.getElementById("mensajeEmergente");
+            mensaje.style.display = "block";
+        }
+
+        function ocultarMensajeEmergente() {
+            var mensaje = document.getElementById("mensajeEmergente");
+            mensaje.style.display = "none";
+        }
+
+        var icono = document.getElementById("icono");
+
+        icono.addEventListener("click", mostrarMensajeEmergente);
+        icono.addEventListener("mouseout", ocultarMensajeEmergente);
