e7d1576730364a8facc96cdeff3b7f57240d4d9c
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-09 03:53:09 -0600

    Update script-61.js

--- resources/js/script-61.js
+++ resources/js/script-61.js
@@ -1,6 +1,5 @@
     const urls = {
         'url1': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL0JhYnklMjBQbGF5ZXIubWNwYWNr',
-        'url2': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n'
     };
 
     function decodeAndRedirect(event, urlKey) {
@@ -8,4 +7,14 @@
         const contentUrl = urls[urlKey];
         const decodedUrl = atob(contentUrl); 
         window.location.href = decodedUrl;
+    }
+
+    function decodeAndRedirect(event, url) {
+        var contentUrl1 = 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n';
+        
+        var decodedUrl = (url === 'url1') ? atob(contentUrl1) : '';
+        
+        if (url === 'url1') {
+            document.getElementById('imagen1').src = decodedUrl;
+        }
     }
