bff20ebe414a820fd19a1511759980315598f1c9
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-09 02:46:10 -0600

    Create script-61.js

---
+++ resources/js/script-61.js
@@ -0,0 +1,10 @@
+        const urls = {
+            'url1': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL0JhYnklMjBQbGF5ZXIubWNwYWNr',
+        };
+
+        function decodeAndRedirect(event, urlKey) {
+            event.preventDefault();
+            const contentUrl = urls[urlKey];
+            const decodedUrl = atob(contentUrl); 
+            window.location.href = decodedUrl;
+        }
