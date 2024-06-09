56865caa58e408e907bb0a008e3d1d0538e37ebc
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-09 03:30:50 -0600

    Update script-61.js

--- resources/js/script-61.js
+++ resources/js/script-61.js
@@ -1,5 +1,6 @@
     const urls = {
-        'url1': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL0JhYnklMjBQbGF5ZXIubWNwYWNr'
+        'url1': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL0JhYnklMjBQbGF5ZXIubWNwYWNr',
+        'url2': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n'
     };
 
     function decodeAndRedirect(event, urlKey) {
@@ -7,16 +8,4 @@
         const contentUrl = urls[urlKey];
         const decodedUrl = atob(contentUrl); 
         window.location.href = decodedUrl;
-    }
-
-    const images = {
-        'img1': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n'
-    };
-
-    function displayImage(event, imageKey) {
-        event.preventDefault();
-        const imgData = images[imageKey];
-        const img = new Image();
-        img.src = imgData;
-        document.body.appendChild(img);
     }
