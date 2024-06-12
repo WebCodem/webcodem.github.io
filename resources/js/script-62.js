88ba3ad08cb1728724bb9f20fff68dc70508bb14
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-11 22:10:36 -0600

    Update script-62.js

--- resources/js/script-62.js
+++ resources/js/script-62.js
@@ -10,8 +10,8 @@
 
         const contentUrl3 = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL0NhbGwgT2YgRHV0eSBNb2Rlcm4gV2FyZmFyZSBJSSAtIHdlYmNvZGVtLnBuZw";  
         const decodedUrl3 = atob(contentUrl3);
-        const imgElement3 = document.getElementById("imagen2");
-        imgElement2.src = decodedUrl3;
+        const imgElement3 = document.getElementById("imagen3");
+        imgElement3.src = decodedUrl3;
         
         const faviconUrl = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2xvZ28ucG5n";
         const decodedFaviconUrl = atob(faviconUrl);
