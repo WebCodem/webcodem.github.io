1e5a5dfb524aa9050667628a79f75280f9e980d6
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-11 22:58:20 -0600

    Update script-62.js

--- resources/js/script-62.js
+++ resources/js/script-62.js
@@ -1,21 +1,22 @@
-        const contentUrl1 = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2xvZ28ucG5n";
-        const decodedUrl1 = atob(contentUrl1);
-        const imgElement1 = document.getElementById("imagen1");
-        imgElement1.src = decodedUrl1;
+document.addEventListener("DOMContentLoaded", function() {
+    const contentUrls = {
+        "imagen1": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2xvZ28ucG5n",
+        "imagen2": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n",
+        "imagen3": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL0NhbGwgT2YgRHV0eSBNb2Rlcm4gV2FyZmFyZSBJSSAtIHdlYmNvZGVtLnBuZw"
+    };
 
-        const contentUrl2 = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n";  
-        const decodedUrl2 = atob(contentUrl2);
-        const imgElement2 = document.getElementById("imagen2");
-        imgElement2.src = decodedUrl2;
+    for (const [id, url] of Object.entries(contentUrls)) {
+        const decodedUrl = atob(url);
+        const imgElement = document.getElementById(id);
+        if (imgElement) {
+            imgElement.src = decodedUrl;
+        }
+    }
 
-        const contentUrl3 = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL0NhbGwgT2YgRHV0eSBNb2Rlcm4gV2FyZmFyZSBJSSAtIHdlYmNvZGVtLnBuZw";  
-        const decodedUrl3 = atob(contentUrl3);
-        const imgElement3 = document.getElementById("imagen3");
-        imgElement3.src = decodedUrl3;
-        
-        const faviconUrl = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2xvZ28ucG5n";
-        const decodedFaviconUrl = atob(faviconUrl);
-        const linkElement = document.createElement('link');
-        linkElement.rel = 'icon';
-        linkElement.href = decodedFaviconUrl;
-        document.head.appendChild(linkElement);
+    const faviconUrl = "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2xvZ28ucG5n";
+    const decodedFaviconUrl = atob(faviconUrl);
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'icon';
+    linkElement.href = decodedFaviconUrl;
+    document.head.appendChild(linkElement);
+});
