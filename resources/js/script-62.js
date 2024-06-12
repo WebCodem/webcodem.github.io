e48e3010024a00b7c85d9863aa7ea049c8ebf020
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-12 00:02:05 -0600

    Update script-62.js

--- resources/js/script-62.js
+++ resources/js/script-62.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const contentUrls = {
         "imagen1": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2xvZ28ucG5n",
         "imagen2": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2JhYnkgcGxheWVyIC0gd2ViY29kZW0ucG5n",
-        "imagen3": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL0NhbGwgT2YgRHV0eSBNb2Rlcm4gV2FyZmFyZSBJSSAtIHdlYmNvZGVtLnBuZw"
+        "imagen3": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL0NhbGwgT2YgRHV0eSBNb2Rlcm4gV2FyZmFyZSBJSSAtIHdlYmNvZGVtLnBuZw",
+        "imagen4": "aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2NhcmdhbmRvLmdpZg:
     };
 
     for (const [id, url] of Object.entries(contentUrls)) {
