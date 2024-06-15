bacb04458a51ccd4be66393e55c0633b278dbed9
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-15 17:21:50 -0600

    Update script-1.js

--- resources/js/script-1.js
+++ resources/js/script-1.js
@@ -1,36 +1,37 @@
-         function actualizarResultados(query) {
-            var resultContainer = document.getElementById("resultContainer");
-            var enlaces = document.getElementsByTagName("a");
+function actualizarResultados(query) {
+    var resultContainer = document.getElementById("resultContainer");
+    var enlaces = document.getElementsByTagName("a");
 
-            if (query.trim() === "") {
-                resultContainer.innerHTML = "";
-                return;
-            }
+    if (query.trim() === "") {
+        resultContainer.innerHTML = "";
+        return;
+    }
 
-            Array.from(resultContainer.children).forEach(function(item) {
-                item.style.display = "none";
+    Array.from(resultContainer.children).forEach(function(item) {
+        item.style.display = "none";
+    });
+
+    for (var i = 0; i < enlaces.length; i++) {
+        var enlace = enlaces[i];
+        var nombreEnlace = enlace.getAttribute("data-name");
+        
+        if (nombreEnlace && nombreEnlace.toLowerCase().includes(query.toLowerCase())) {
+            var resultItem = document.createElement("div");
+            resultItem.classList.add("result-item");
+            resultItem.textContent = nombreEnlace;
+            resultItem.addEventListener("click", function() {
+                window.location.href = enlace.href; 
             });
-
-            for (var i = 0; i < enlaces.length; i++) {
-                var enlace = enlaces[i];
-                var nombreEnlace = enlace.getAttribute("data-name");
-                if (nombreEnlace.toLowerCase().includes(query.toLowerCase())) {
-                    var resultItem = document.createElement("div");
-                    resultItem.classList.add("result-item");
-                    resultItem.textContent = nombreEnlace;
-                    resultItem.addEventListener("click", function() {
-                        window.location.href = enlace.href; 
-                    });
-                    resultContainer.appendChild(resultItem);
-                    resultItem.style.display = "block"; 
-                    break; 
-                }
-            }
+            resultContainer.appendChild(resultItem);
+            resultItem.style.display = "block"; 
+            break; 
         }
+    }
+}
 
-        var searchInput = document.getElementById("searchInput");
+var searchInput = document.getElementById("searchInput");
 
-        searchInput.addEventListener("input", function() {
-            var query = this.value;
-            actualizarResultados(query);
-        });
+searchInput.addEventListener("input", function() {
+    var query = this.value;
+    actualizarResultados(query);
+});
