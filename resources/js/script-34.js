9b1d0ce77949afbfaa1a852231b5b80ec627db85
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-15 01:53:52 -0600

    Update script-34.js

--- resources/js/script-34.js
+++ resources/js/script-34.js
@@ -1,30 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    if (localStorage.getItem("loggedIn") === "true") {
-      document.querySelector(".overlay").style.display = "none";
+  if (localStorage.getItem("loggedIn") === "true") {
+    document.querySelector(".overlay").style.display = "none";
+  } else {
+    document.querySelector(".overlay").style.display = "flex";
+  }
+});
+
+const users = [
+  { username: "R2FkZGllbA", password: "MTIxMTk4MjA4" },
+  { username: "VGVzdA", password: "VGVzdA" }
+];
+
+const bannedUsers = ["VGVzdA"]; 
+
+function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  const user = users.find(u => atob(u.username) === username && atob(u.password) === password);
+
+  if (user) {
+    if (bannedUsers.includes(user.username)) {
+      alert('Este usuario está baneado.');
     } else {
-      document.querySelector(".overlay").style.display = "flex";
-    }
-  });
-
-  const users = [
-    { username: "R2FkZGllbA", password: "MTIxMTk4MjA4" },
-    { username: "VGVzdA", password: "VGVzdA" }
-  ];
-
-  function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const user = users.find(u => atob(u.username) === username && atob(u.password) === password);
-
-    if (user) {
       localStorage.setItem("loggedIn", "true");
       closePopup();
-    } else {
-      alert('Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.');
     }
+  } else {
+    alert('Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.');
   }
+}
 
-  function closePopup() {
-    document.querySelector('.overlay').style.display = 'none';
-  }
+function closePopup() {
+  document.querySelector('.overlay').style.display = 'none';
+}
