ee9262c6a6808f58c6ebfd2525da2487e2316044
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-15 16:25:47 -0600

    Update script-34.js

--- resources/js/script-34.js
+++ resources/js/script-34.js
@@ -1,65 +1,65 @@
 document.addEventListener("DOMContentLoaded", function() {
-  if (localStorage.getItem("loggedIn") === "true") {
-    const currentUserBase64 = localStorage.getItem("currentUser");
-    const currentUser = currentUserBase64 ? atob(currentUserBase64) : null;
-    if (currentUser && bannedUsers.includes(currentUser)) {
-      alert('Tu cuenta ha sido baneada.');
-      localStorage.removeItem("loggedIn");
-      localStorage.removeItem("currentUser");
-      document.querySelector(".overlay").style.display = "flex";
+    if (localStorage.getItem("loggedIn") === "true") {
+        const currentUserBase64 = localStorage.getItem("currentUser");
+        const currentUser = currentUserBase64 ? atob(currentUserBase64) : null;
+        if (currentUser && bannedUsers.includes(currentUser)) {
+            alert('Tu cuenta ha sido baneada.');
+            localStorage.removeItem("loggedIn");
+            localStorage.removeItem("currentUser");
+            document.querySelector(".overlay").style.display = "flex";
+        } else {
+            document.querySelector(".overlay").style.display = "none";
+        }
     } else {
-      document.querySelector(".overlay").style.display = "none";
+        document.querySelector(".overlay").style.display = "flex";
     }
-  } else {
-    document.querySelector(".overlay").style.display = "flex";
-  }
 });
 
 const users = [
-  { username: "R2FkZGllbA", password: "MTIxMTk4MjA4" }, // "Gaddiel" : "121198208"
-  { username: "VGVzdA", password: "VGVzdA" } // "Test" : "Test"
+    { username: "R2FkZGllbA", password: "MTIxMTk4MjA4" }, 
+    { username: "VGVzdA", password: "VGVzdA" } 
 ];
 
-let bannedUsers = [""]; 
+let bannedUsers = ["VGVzdA"]; 
 
 function login() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-  const user = users.find(u => atob(u.username) === username && atob(u.password) === password);
+    const user = users.find(u => atob(u.username) === username && atob(u.password) === password);
 
-  if (user) {
-    if (bannedUsers.includes(atob(user.username))) {
-      alert('Este usuario está baneado.');
+    if (user) {
+        if (bannedUsers.includes(atob(user.username))) {
+            alert('Este usuario está baneado.');
+        } else {
+            localStorage.setItem("loggedIn", "true");
+            localStorage.setItem("currentUser", user.username);
+            closePopup();
+
+            const profilePic = localStorage.getItem('profilePic') || 'https://webcodem-media.github.io/resources/multimedia/imagen/perfil-predeterminado.jpg';
+            displayUserData(username, profilePic);
+        }
     } else {
-      localStorage.setItem("loggedIn", "true");
-      localStorage.setItem("currentUser", user.username);
-      closePopup();
-
-      const profilePic = localStorage.getItem('profilePic') || 'https://webcodem-media.github.io/resources/multimedia/imagen/perfil-predeterminado.jpg';
-      displayUserData(username, profilePic); // Update user data after login
+        alert('Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.');
     }
-  } else {
-    alert('Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.');
-  }
 }
 
 function closePopup() {
-  document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
 }
 
 function banUser(username) {
-  if (!bannedUsers.includes(username)) {
-    bannedUsers.push(username);
-    if (localStorage.getItem("loggedIn") === "true") {
-      const currentUserBase64 = localStorage.getItem("currentUser");
-      const currentUser = currentUserBase64 ? atob(currentUserBase64) : null;
-      if (currentUser === username) {
-        alert('Tu cuenta ha sido baneada.');
-        localStorage.removeItem("loggedIn");
-        localStorage.removeItem("currentUser");
-        document.querySelector(".overlay").style.display = "flex";
-      }
+    if (!bannedUsers.includes(username)) {
+        bannedUsers.push(username);
+        if (localStorage.getItem("loggedIn") === "true") {
+            const currentUserBase64 = localStorage.getItem("currentUser");
+            const currentUser = currentUserBase64 ? atob(currentUserBase64) : null;
+            if (currentUser === username) {
+                alert('Tu cuenta ha sido baneada.');
+                localStorage.removeItem("loggedIn");
+                localStorage.removeItem("currentUser");
+                document.querySelector(".overlay").style.display = "flex";
+            }
+        }
     }
-  }
 }
