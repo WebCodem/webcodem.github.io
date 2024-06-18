925732ead8d0d8dfcd580a1586a45d40cea510d8
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-18 03:26:52 -0600

    Update script-61.js

--- resources/js/script-61.js
+++ resources/js/script-61.js
@@ -3,7 +3,7 @@
         'url2': 'aHR0cHM6Ly93ZWJjb2RlbS5naXRodWIuaW8vZGVzY2FyZ2FyLWVycm9yLTQwNA',
         'url3': 'aHR0cHM6Ly93ZWJjb2RlbS5naXRodWIuaW8vcCVDMyVBMWdpbmEtZXJyb3ItNDA0',
         'url4': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL2FjdGlvbnMtc3R1ZmYtMS0wLTIubWNwYWNr',
-        'url5': 'aHR0cDovL3dlYmNvZGVtLW1lZGlhLmdpdGh1Yi5pby9yZXNvdXJjZXMvbXVsdGltZWRpYS9tYXJrZXRwbGFjZS9taW5lY3JhZnQvdGV4dHVyYS9CYXJlJTIwQm9uZSUyMFBsdXMlMjBbQkVUQV0ubWNwYWNr',
+        'url5': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL0JhcmUgQm9uZSBQbHVzIFtCRVRBXS5tY3BhY2s',
     };
 
     function decodeAndRedirect(event, urlKey) {
