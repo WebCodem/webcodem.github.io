3f2b7ed04635ba412dc95e2f2cc1ed1d6eec546b
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-18 03:18:16 -0600

    Update script-61.js

--- resources/js/script-61.js
+++ resources/js/script-61.js
@@ -3,6 +3,7 @@
         'url2': 'aHR0cHM6Ly93ZWJjb2RlbS5naXRodWIuaW8vZGVzY2FyZ2FyLWVycm9yLTQwNA',
         'url3': 'aHR0cHM6Ly93ZWJjb2RlbS5naXRodWIuaW8vcCVDMyVBMWdpbmEtZXJyb3ItNDA0',
         'url4': 'aHR0cHM6Ly93ZWJjb2RlbS1tZWRpYS5naXRodWIuaW8vcmVzb3VyY2VzL21hcmtldHBsYWNlL21pbmVjcmFmdC90ZXh0dXJhL2FjdGlvbnMtc3R1ZmYtMS0wLTIubWNwYWNr',
+        'url5': 'aHR0cDovL3dlYmNvZGVtLW1lZGlhLmdpdGh1Yi5pby9yZXNvdXJjZXMvbXVsdGltZWRpYS9tYXJrZXRwbGFjZS9taW5lY3JhZnQvdGV4dHVyYS9CYXJlJTIwQm9uZSUyMFBsdXMlMjBbQkVUQV0ubWNwYWNr',
     };
 
     function decodeAndRedirect(event, urlKey) {
