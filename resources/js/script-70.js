15455c48e733c1196d16c9b5587756fdcdb00a2d
Gaddiel <151910209+WebCodem@users.noreply.github.com>
2024-06-22 23:08:53 -0600

    Update script-70.js

--- resources/js/script-70.js
+++ resources/js/script-70.js
@@ -1,5 +1,5 @@
 var idioma = navigator.language || navigator.userLanguage; 
 
         if (idioma.startsWith('en')) {
-            window.location.href = 'https://webcodem.github.io/marketplace/actions-&-stuff-en-us'; 
+            window.location.href = 'https://webcodem.github.io/marketplace-en-us/actions-&-stuff'; 
         }
